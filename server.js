fde554604ba79f040f7584d5ceb636c78e1decc0
harishkumarsp <harishkumarsp1998@gmail.com>
2025-01-10 22:48:19 +0530

    added mysql db by replacing file-system

--- server.js
+++ server.js
@@ -1,17 +1,22 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-// const { engine } = require('express-handlebars')
 
 const app = express();
+const db = require("./utils/database");
 
-// to use handle bar as a engine
-// app.engine('hbs', engine({ extname: 'hbs', }))
-// app.set('view engine', 'hbs'y)
 app.set("view engine", "ejs");
 
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 const { get404 } = require("./controllers/error");
+
+db.execute("SELECT * FROM products")
+	.then(result => {
+		console.log(result[0], result[1]);
+	})
+	.catch(err => {
+		console.log(err);
+	});
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static("public"));
