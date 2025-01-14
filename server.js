a84b24fbb7394a8b87fe97c2a88eae8c3f7f66ce
harishkumarsp <harishkumarsp1998@gmail.com>
2025-01-14 12:56:45 +0530

    chore: Update server.js with Sequelize ORM and database associations

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
-const db = require("./utils/database");
+const { sequelize } = require("./utils/database");
 
 app.set("view engine", "ejs");
 
@@ -10,22 +10,60 @@ const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 const { get404 } = require("./controllers/error");
 
-db.execute("SELECT * FROM products")
-	.then(result => {
-		console.log(result[0], result[1]);
-	})
-	.catch(err => {
-		console.log(err);
-	});
+const Product = require("./models/product");
+const User = require("./models/user");
+const Cart = require("./models/cart");
+const CartItem = require("./models/cartItem");
+const Order = require("./models/order");
+const OrderItem = require("./models/orderItem");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static("public"));
+
+app.use((req, res, next) => {
+	User.findByPk(1)
+		.then(user => {
+			req.user = user;
+			next();
+		})
+		.catch(err => console.log(err));
+});
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use(get404);
 
-app.listen(4000, () => {
-	console.log("App is running on 4000");
-});
+// Database associations(relations)
+Product.belongsTo(User, { constraints: true, onDelete: "CASCADE" });
+User.hasMany(Product);
+User.hasOne(Cart);
+Cart.belongsTo(User);
+Cart.belongsToMany(Product, { through: CartItem });
+Product.belongsToMany(Cart, { through: CartItem });
+Order.belongsTo(User);
+User.hasMany(Order);
+Order.belongsToMany(Product, { through: OrderItem });
+
+sequelize
+	.sync()
+	.then(() => {
+		return User.findByPk(1);
+	})
+	.then(user => {
+		if (!user) {
+			return User.create({ name: "Harish", email: "test@gmail.com" });
+		}
+		return user;
+	})
+	.then(user => {
+		return user.createCart();
+	})
+	.then(data => {
+		app.listen(4000, () => {
+			console.log("App is running on 4000");
+		});
+	})
+	.catch(err => {
+		console.log(err);
+	});
